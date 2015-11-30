6eaf8293db8fe84ff7837968d1a2a45d0cd3b336
modshrink <mayoibi@modshrink.com>
2015-11-30 13:07:45 +0900

    add: テンプレートファイル

--- core/login.class.php
+++ core/login.class.php
@@ -46,42 +46,13 @@ class Login {
 
 $login = new Login();
 ?>
-<html>
-<head>
-	<title>pm</title>
-	<link href="../css/bootstrap.min.css" rel="stylesheet">
-	<link href="../css/style.css" rel="stylesheet">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-	<script src="../js/bootstrap.min.js"></script>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
-<body>
+<?php
+$alert = $login->db_connect_check_message();
+$alert .= ssl_message();
+?>
 
-<nav class="navbar navbar-inverse navbar-fixed-top">
-<div class="container">
-<div class="navbar-header">
-  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-    <span class="sr-only">Toggle navigation</span>
-    <span class="icon-bar"></span>
-    <span class="icon-bar"></span>
-    <span class="icon-bar"></span>
-  </button>
-  <a class="navbar-brand" href="#">Project name</a>
-</div>
-<div id="navbar" class="collapse navbar-collapse">
-  <ul class="nav navbar-nav">
-    <li class="active"><a href="#">Home</a></li>
-    <li><a href="#about">About</a></li>
-    <li><a href="#contact">Contact</a></li>
-  </ul>
-</div><!--/.nav-collapse -->
-</div>
-</nav>
+<?php
+$contents = file_get_contents( '../tpl/index.tpl' );
+$contents = str_replace( '<%CONTENTS%>', $alert, $contents );
 
-
-<div class="container">
-<?php $login->db_connect_check_message(); ?>
-<?php ssl_message(); ?>
-</div> <!-- .container -->
-</body>
-</head>
+echo $contents;
