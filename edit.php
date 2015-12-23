bd6e64d120949973857a121f518c7b5e3104163b
mayoibi <mayoibi@modshrink.com>
2015-12-24 04:12:23 +0900

    個別editモード

--- edit.php
+++ edit.php
@@ -1,22 +1,48 @@
 <?php
-require_once( dirname(__FILE__) . '/core/edit.class.php' );
-
-$edit = new Edit();
+require_once( dirname(__FILE__) . '/core/DB.class.php' );
 $DB = new DB();
+
+$data_id = esc_html( $_GET['id'] );
 
 $alert = $DB->db_connect_check_message();
 $alert .= ssl_message();
 
-$input_array = array();
-foreach( input_data_list() as $input ) {
-	$input_array[$input] = esc_html( $_POST[$input] );
+if( $data_id ) {
+	// 編集モード
+	$input_array = array();
+	foreach( input_data_list() as $input ) {
+		$input_array[$input] = esc_html( $_POST[$input] );
+	}
+
+	if( $_POST['submit'] ) {
+		$DB->update_data( $data_id, $input_array );
+	}
+} else {
+	// 新規作成モード
+	$input_array = array();
+	foreach( input_data_list() as $input ) {
+		$input_array[$input] = esc_html( $_POST[$input] );
+	}
+
+	if( $_POST['submit'] ) {
+		$DB->insert_data( $input_array );
+	}
 }
 
-if( $_POST['submit'] ) {
-	$DB->insert_data( $input_array );
+$content = file_get_contents( dirname(__FILE__) . '/tpl/edit.tpl' );
+//$contents = str_replace( '<%CONTENTS%>', $alert, $content );
+
+$tag_key = array();
+$tag_value = array();
+
+foreach ( $DB->get_register_data_array( $data_id ) as $key => $value ) {
+	$tag_key[] = '<%' . $key . '%>';
+	$tag_value[] = $value;
 }
 
-$contents = file_get_contents( dirname(__FILE__) . '/tpl/edit.tpl' );
-$contents = str_replace( '<%CONTENTS%>', $alert, $contents );
+//var_dump( $tag_key);
+//var_dump( $tag_value);
+
+$contents .= str_replace( $tag_key, $tag_value, $content );
 
 echo $contents;
