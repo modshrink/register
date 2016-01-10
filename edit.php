1da9e54a8dcdb247d4949e050b90a418fa9af86d
modshrink <mayoibi@modshrink.com>
2016-01-10 16:26:59 +0900

    新規編集モード修正中

--- edit.php
+++ edit.php
@@ -2,11 +2,14 @@
 require_once( dirname(__FILE__) . '/core/DB.class.php' );
 $DB = new DB();
 
+// URLのIDパラメータを取得
 $data_id = esc_html( $_GET['id'] );
 
+// アラートを表示
 $alert = $DB->db_connect_check_message();
 $alert .= ssl_message();
 
+// IDパラメータの有無でモード変更
 if( $data_id ) {
 	// 編集モード
 	$input_array = array();
@@ -19,14 +22,7 @@ if( $data_id ) {
 	}
 } else {
 	// 新規作成モード
-	$input_array = array();
-	foreach( input_data_list() as $input ) {
-		$input_array[$input] = esc_html( $_POST[$input] );
-	}
-
-	if( $_POST['submit'] ) {
-		$DB->insert_data( $input_array );
-	}
+	echo '新規作成だよ';
 }
 
 $content = file_get_contents( dirname(__FILE__) . '/tpl/edit.tpl' );
@@ -39,9 +35,6 @@ foreach ( $DB->get_register_data_array( $data_id ) as $key => $value ) {
 	$tag_key[] = '<%' . $key . '%>';
 	$tag_value[] = $value;
 }
-
-//var_dump( $tag_key);
-//var_dump( $tag_value);
 
 $contents .= str_replace( $tag_key, $tag_value, $content );
 
